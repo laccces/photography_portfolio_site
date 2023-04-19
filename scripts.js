cd6eee25f3a2ce5a35ca1b32a5bf6c63d344a509
laccces <en07djc@icloud.com>
2023-04-19 15:09:43 +0100

    Changed break points and slider on mobile

--- scripts.js
+++ scripts.js
@@ -3,9 +3,11 @@ let currentSlide = 0;
 const slideInterval = 9000; // Change this value to set the interval between slides (in milliseconds)
 
 function nextSlide() {
-    slides[currentSlide].style.opacity = 0;
-    currentSlide = (currentSlide + 1) % slides.length;
-    slides[currentSlide].style.opacity = 1;
+    if (window.innerWidth >= 800) {
+        slides[currentSlide].style.opacity = 0;
+        currentSlide = (currentSlide + 1) % slides.length;
+        slides[currentSlide].style.opacity = 1;
+    }
 }
 
 setInterval(nextSlide, slideInterval);
